9cfdd75fa0753667d2b9c7d34aeef82aefe4c54c
Tim Rooney <timothyrooney93@gmail.com>
2017-04-18 16:08:46 -0500

    Updated main file to test async changes

--- ballistabot/ballistabot.ino
+++ ballistabot/ballistabot.ino
@@ -3,25 +3,17 @@
 #include "hopper.h"
 #include "Servo.h"
 
-//Servo _leftMotor;
-//Servo _rightMotor;
 
-// Driver driver(2, 3);
 Hopper hopper(10, 12);
 
 void setup() {
   Serial.begin(9600);
-  // driver.init();
   hopper.init();
+  hopper.load();
 }
 
 void loop() {
-//  driver.forward(0.5);
-//  delay(3000);
-//  driver.backward(0.5);
-//  delay(3000);
-//  driver.turn(CLOCKWISE, 0.5);
-//  delay(3000);
-  delay(5000);
-  hopper.load();
+  hopper.update();
+  Serial.println("Hi");
+  delay(250);
 }
