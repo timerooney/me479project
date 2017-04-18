138cca2d8b1ee550f598ea5e4519c69e31a373a1
Tim Rooney <timothy.e.rooney@gmail.com>
2017-04-17 23:19:19 -0500

    Updated hopper to use timing methods, untested

--- ballistabot/hopper.h
+++ ballistabot/hopper.h
@@ -8,12 +8,18 @@ class Hopper {
 public:
   Hopper(int releaseMotorPin, int lockMotorPin);
   void init();
+  void update();
   void load();
 private:
   int _releaseMotorPin;
   int _lockMotorPin;
   Servo _releaseMotor;
   Servo _lockMotor;
+
+  // Keep track of hopper loading state
+  void _loadUpdate();
+  int _loadState;
+  unsigned long _nextLoadTime;
 };
 
 #endif
