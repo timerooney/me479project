06baf7253f5c94e47f63ce1dfa6c065503fd5f38
Tim Rooney <timothyrooney93@gmail.com>
2017-05-12 10:53:34 -0500

    Prep for merge

--- src/main.cpp
+++ src/main.cpp
@@ -38,33 +38,32 @@ void setup() {
 }
 
 void loop() {
-  windingmotor.windBack();
-  // delay(100);
-  // x_pos = vision.get_x_pos();
-  // Serial.println(x_pos);
-  // if (x_pos < 160 && x_pos >= 0) {
-  //   driver.turn(CLOCKWISE, 0.3);
-  // } else if (x_pos > 200) {
-  //   driver.turn(COUNTERCLOCKWISE, 0.3);
-  // } else if (x_pos >= 0) {
-  //   driver.forward(0.25);
-  //   // Fire if possible
-  //   if (hopper.is_loaded == 1) {
-  //     driver.stop();
-  //     Serial.println("Fire!");
-  //     hopper.is_loaded = 0;
-  //   }
-  // } else {
-  //   driver.stop();
-  // }
-  // 
-  // // Reload the hopper if it is not loaded
-  // if (hopper.is_loaded == 0 && hopper.is_reloading == 0) {
-  //   Serial.println("Reloading...");
-  //   hopper.load();
-  // }
+  delay(100);
+  x_pos = vision.get_x_pos();
+  Serial.println(x_pos);
+  if (x_pos < 160 && x_pos >= 0) {
+    driver.turn(CLOCKWISE, 0.3);
+  } else if (x_pos > 200) {
+    driver.turn(COUNTERCLOCKWISE, 0.3);
+  } else if (x_pos >= 0) {
+    driver.forward(0.25);
+    // Fire if possible
+    if (hopper.is_loaded == 1) {
+      driver.stop();
+      Serial.println("Fire!");
+      hopper.is_loaded = 0;
+    }
+  } else {
+    driver.stop();
+  }
+  
+  // Reload the hopper if it is not loaded
+  if (hopper.is_loaded == 0 && hopper.is_reloading == 0) {
+    Serial.println("Reloading...");
+    hopper.load();
+  }
 
-  // // Refresh devices as needed
-  // hopper.update();
+  // Refresh devices as needed
+  hopper.update();
 }
 
